32e9d176330e9c54fd052b1d79ceafa1bd9f68ee
yerim216 <bing_01@naver.com>
2023-07-17 21:02:01 +0900

    Chore: rds 연결을 위한 db정보 추가, .gitignore파일 수정

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+var mysql = require('mysql');
+const db = require('./lib/database')
 
 require("dotenv").config();
 
-const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
+const port = 5000;
 
 const app = express();
 
@@ -11,6 +14,16 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", require("./src"));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+//rds connect
+const database = require('./module/db_connect');
+const conn = database.conn();
+
+
+ app.listen(port, () => {
+   console.log(`Server is running on port ${port}`);
+ });
+
+
+
+
+ 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-const express = require("express");
-const router = express.Router();
+var express = require("express");
+var router = express.Router();
+
 
 router.use(express.json());
 
 const mypageRouter = require("./api/mypage/mypage.index"); //맡은 도메인 연결
+//const loginRouter = require("./api/oauth/google.js")
 
 router.use("/mypage", mypageRouter);
+//router.use("/oauth", loginRouter)
+//router.use('/db', require('./lib/database.js'))
 
 module.exports = router;
