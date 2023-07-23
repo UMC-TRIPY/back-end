539371ad35f5a152c535b62b202e00e3b0480c7b
yerim216 <bing_01@naver.com>
2023-07-23 18:41:59 +0900

    Chore : rds .gitignore 수정, rds 테스트를 위한 server.js 파일 수정

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
+
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
@@ -10,6 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", require("./src"));
+
+db.conn();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
