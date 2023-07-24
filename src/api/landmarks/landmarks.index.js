e047fae111a886b14a7986ac76ad3372334bc306
p_yool <80208160+p-yool@users.noreply.github.com>
2023-07-24 19:46:55 +0900

    fix. delete not using trash code

--- src/api/landmarks/landmarks.index.js
+++ src/api/landmarks/landmarks.index.js
@@ -1,9 +1,6 @@
 const router = require("express").Router();
 const landmarkController = require("./landmarks.controller");
 
-router.get("/", function (req, res) {
-  res.send("Hello World");
-});
 
 router.get("/search-landmarks", landmarkController.searchLandmark);
 
