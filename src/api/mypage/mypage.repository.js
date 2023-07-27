e9a3ddf6e3ff9fe37fe9a73a9cb3f61751f319b2
박제영 <130127934+Zero982@users.noreply.github.com>
2023-07-27 19:08:39 +0900

    [FEAT] (#8)
    1.내 정보 조회 API
    2.회원 탈퇴 API
    3.프로필 등록 API
    4.프로필 삭제 API
    5.국적 등록 API
    6.국적 수정 API
    7.국적 삭제 API

--- src/api/mypage/mypage.repository.js
+++ src/api/mypage/mypage.repository.js
@@ -150,3 +150,122 @@ exports.breakFriend = (user_idx, friend_idx) => {
     );
   });
 };
+
+// 유저 정보 조회 API
+exports.getUserByInfoId = (kakaoId) => {
+  return new Promise((resolve, reject) => {
+      mysqlConnection.query(
+                  `
+                  SELECT nickname, email, nationality, profileImg 
+                  FROM user  
+                  WHERE kakaoId = ${kakaoId}`,
+      (err,result) => {
+      if(err){
+           reject(err);
+       }else {
+          resolve(result);
+      }
+      }
+      );
+  });
+};
+  
+// 유저 탈퇴 API
+exports.deleteUser = (kakaoId) =>{
+  return new Promise((resolve, reject) =>{
+      mysqlConnection.query(
+                  `
+                  DELETE
+                  FROM user
+                  WHERE kakaoId = ${kakaoId}`,
+
+      (err,result) => {
+      if(err) reject(err);
+      resolve(true);
+      }
+      );
+  });
+};
+
+
+
+//profile 이미지 등록 API
+exports.saveProfileImage = (kakaoId, profileImg) => {
+  return new Promise((resolve, reject) => {
+  mysqlConnection.query(
+  `UPDATE user 
+   SET profileImg = ? 
+   WHERE kakaoId = ?`,[profileImg,kakaoId],
+  (err, result) => {
+  if (err) reject(err);
+  resolve(true);
+          }
+      );
+  });
+};
+
+//Profile 이미지 삭제 API
+exports.deleteProfileImage = (kakaoId) => {
+  return new Promise((resolve, reject) => {
+      mysqlConnection.query(
+          `UPDATE user 
+           SET profileImg = NULL
+           WHERE kakaoId = ${kakaoId}
+           `,
+  (err, result) => {
+      if(err) reject(err);
+  resolve(true);
+      }
+      );
+  });
+};
+
+// 국적 등록 API
+exports.saveNationality = (kakaoId, nationality) => {
+  return new Promise((resolve, reject) => {
+  mysqlConnection.query(
+                  `
+                  UPDATE user 
+                  SET nationality = ? 
+                  WHERE kakaoId = ?`,[nationality,kakaoId],
+   (err, result) => {
+    if (err) reject(err);
+    resolve(true);
+          }
+       );
+  });
+};
+
+//국적 수정 API
+exports.updateNationality = (kakaoId,nationality) =>{
+  return new Promise((resolve, reject) => {
+      mysqlConnection.query(
+          `UPDATE user
+           SET nationality = ?
+           WHERE kakaoId = ?
+           `,[nationality,kakaoId],
+  (err, result) => {
+  if(err) reject(err);
+  resolve(true);
+      }
+      );
+  });
+};
+
+
+//국적 삭제 API
+exports.deleteNationality = (kakaoId) =>{
+  return new Promise((resolve, reject) => {
+      mysqlConnection.query(
+          `UPDATE user
+           SET nationality = NULL
+           WHERE kakaoId = ${kakaoId}
+           `,
+  (err, result) => {
+      if(err) reject(err);
+  
+  resolve(true);
+      }
+      );
+  });
+};
--- src/api/mypage/mypage.service.js
+++ src/api/mypage/mypage.service.js
@@ -79,3 +79,79 @@ exports.breakFriend = async (user_idx, friend_idx) => {
     throw err;
   }
 };
+
+// 유저 정보 조회 API
+exports.getUserByInfoId = async (kakaoId) => {
+  try {
+      const result = await myPageRepository.getUserByInfoId(kakaoId);
+      return result;
+
+  } catch (err) {
+      throw err;
+  }
+};
+
+//유저 삭제 API
+exports.deleteUser = async (kakaoId) => {
+  try{
+      const result = await myPageRepository.deleteUser(kakaoId);
+      return result;
+  } catch (err){
+      throw err;
+  }
+};
+
+// profile Img 등록 API
+exports.saveProfileImage = async (kakaoId, profileImg) => {
+  try {
+  await myPageRepository.saveProfileImage(kakaoId, profileImg);
+  return;
+
+  } catch (err) {
+  throw err;
+  }
+};
+
+//profile Img 삭제 API
+exports.deleteProfileImage = async(kakaoId) => {
+  try{
+      await myPageRepository.deleteProfileImage(kakaoId);
+      return;
+
+  } catch(err){
+      throw err;
+  }
+}
+
+// 국적 등록  API
+exports.saveNationality = async (kakaoId, nationality) => {
+  try {
+  await myPageRepository.saveNationality(kakaoId, nationality);
+  return;
+
+  } catch (err) {
+  throw err;
+  }
+};
+
+// 국적 수정 API
+exports.updateNationality = async (kakaoId, nationality) => {
+  try {
+  await myPageRepository.updateNationality(kakaoId, nationality);
+  return;
+
+  } catch (err) {
+  throw err;
+  }
+};
+
+//국적 삭제 API
+exports.deleteNationality = async(kakaoId) => {
+  try{
+      await myPageRepository.deleteNationality(kakaoId);
+      return;
+
+  } catch(err){
+      throw err;
+  }
+}
