d53b635d90243863fd10f5901bce6a9a46d71870
yerim216 <bing_01@naver.com>
2023-08-01 20:28:54 +0900

    FIx : swagger 배포 cors 에러 해결 -  swagger/swagger.js 에 배포 서버 주소 추가

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -12,8 +12,14 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:5000", // 요청 URL
+        url: "http://tripy.site:5000", // 요청 URL
       },
+      {
+        url : "http://52.79.126.200:5000"
+      },
+      {
+        url : "http://localhost:5000"
+      }
     ],
   },
   apis: ["./src/*.js", "./src/api/**/*.js"], //Swagger 파일 연동
